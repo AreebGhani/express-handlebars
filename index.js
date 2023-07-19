8c3370ec05ffac406b43f23f603e99bf2006175e
AreebGhani <areebghanidps@gmail.com>
2023-07-20 02:09:44 +0500

    updated

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express");
-const app = express();
 const exphbs = require("express-handlebars");
 const path = require("path");
+const router = require("./routes/routes");
 
+const app = express();
 const port = process.env.PORT || 3000;
 
+// Express-Handlebars
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
 app.use(express.static(path.join(__dirname, "static")));
-app.use("/", require(path.join(__dirname, "routes/routes")));
+app.use("/", router);
 
 app.listen(port, () => {
-  console.log(`Server runnimg at port ${port} => http://localhost:${port}`);
+  console.log(`Server running at port ${port} => http://localhost:${port}`);
 });
